2d60972e1febcfba44a2c981b2717f7c270e7a41
ja-key <76249996+ja-key@users.noreply.github.com>
2022-03-09 10:34:53 +0100

    created Fourier subclass

--- damage_identification/features/fourier_transform.py
+++ damage_identification/features/fourier_transform.py
@@ -2,3 +2,9 @@ import pandas as pd
 import numpy as np
 from scipy.fft import fft, ifft
 
+from base import FeatureExtractor
+
+class Fourier(FeatureExtractor):
+    def __init__(self, name: str, params: Dict[str, Any]):
+        self.name = name
+        self.params = params
