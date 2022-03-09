f4d989d8daab4764d017b61d0b4df4a11a8bc2d4
Bo <99323127+bolee02@users.noreply.github.com>
2022-03-09 11:12:16 +0100

    Implemented loading of uncompressed data

--- damage_identification/io.py
+++ damage_identification/io.py
@@ -1,9 +1,17 @@
 import numpy as np
-
+import os
 
 def load_uncompressed_data(filename: str) -> np.ndarray:
-    pass
+    """
+    Args:
+        filename: data file name including file extension
 
+    Returns:
+        Numpy array of the data in the data file
+    """
+    data = np.loadtxt(f"data\{filename}", delimiter = ",")
+    return data
 
 def load_compressed_data(filename: str) -> np.ndarray:
+
     raise NotImplementedError()
