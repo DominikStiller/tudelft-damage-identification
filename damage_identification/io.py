63b8c546d7d4302091b68ec1e5712bdbc9c42c31
Dominik Stiller <domi.stiller@gmail.com>
2022-03-09 11:39:37 +0100

    Use OS-agnostic path joining for data file loading

--- damage_identification/io.py
+++ damage_identification/io.py
@@ -1,6 +1,9 @@
+import os
+
 import numpy as np
 import pandas as pd
 import time
+
 
 def load_uncompressed_data(filename: str) -> np.ndarray:
     """
@@ -10,16 +13,17 @@ def load_uncompressed_data(filename: str) -> np.ndarray:
     Returns:
         Numpy array of the data in the data file
     """
-    data = (pd.read_csv(f"data\{filename}")).to_numpy()
+    data = (pd.read_csv(os.path.join("data", filename))).to_numpy()
     return data
+
 
 def load_compressed_data(filename: str) -> np.ndarray:
     """
-        Args:
-            filename: data file name including file extension
+    Args:
+        filename: data file name including file extension
 
-        Returns:
-            Numpy array of the data in the data file
-        """
-    data = (pd.read_csv(f"data\{filename}", compression = "zip")).to_numpy()
+    Returns:
+        Numpy array of the data in the data file
+    """
+    data = (pd.read_csv(os.path.join("data", filename), compression="zip")).to_numpy()
     return data
