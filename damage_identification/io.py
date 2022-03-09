2f6adfc171cdcde3bc1b45ee42287141326d07d6
Dominik Stiller <domi.stiller@gmail.com>
2022-03-09 10:20:37 +0100

    Add data loading stub

---
+++ damage_identification/io.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def load_uncompressed_data(filename: str) -> np.ndarray:
+    pass
+
+
+def load_compressed_data(filename: str) -> np.ndarray:
+    raise NotImplementedError()
