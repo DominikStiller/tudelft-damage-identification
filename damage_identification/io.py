87f74bf7bfeb0e181953a445ee5398017b705a60
Bo <99323127+bolee02@users.noreply.github.com>
2022-03-11 11:21:33 +0100

    add uncompressed data extraction

--- damage_identification/io.py
+++ damage_identification/io.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
-
+import vallenae as vae
 
 def load_uncompressed_data(filename: str) -> np.ndarray:
     """
@@ -23,6 +23,6 @@ def load_compressed_data(filename: str) -> np.ndarray:
     Returns:
         Numpy array of the data in the data file: n_examples x n_samples
     """
-    # TODO update when compression type is known
-    data = np.transpose((pd.read_csv(os.path.join("data", filename))).to_numpy())
+    compressed_data = vae.io.TraDatabase(os.path.join("data", filename))
+    data = np.vstack(compressed_data.read()["data"].to_numpy())
     return data
