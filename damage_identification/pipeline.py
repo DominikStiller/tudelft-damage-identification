083af9edd104c1087a1e80e5fee87fcf509b0396
Dominik Stiller <domi.stiller@gmail.com>
2022-03-11 11:57:07 +0100

    Add pipeline support for invalid features

--- damage_identification/pipeline.py
+++ damage_identification/pipeline.py
@@ -12,24 +12,13 @@ from damage_identification.features.direct import DirectFeatureExtractor
 from damage_identification.io import load_uncompressed_data, load_compressed_data
 
 
-class TestClu(Clustering):
-    def __init__(self, params: Dict[str, Any]):
-        super(TestClu, self).__init__("testclusterer", params)
-
-    def train(self, examples: pd.DataFrame):
-        pass
-
-    def predict(self, example: pd.DataFrame) -> DamageMode:
-        return DamageMode.DELAMINATION
-
-
 class Pipeline:
     PIPELINE_PERSISTENCE_FOLDER = "data/pipeline/"
 
     def __init__(self, params: Dict[str, Any]):
         self.params = params
         self.feature_extractors: List[FeatureExtractor] = [DirectFeatureExtractor(params)]
-        self.clusterers: List[Clustering] = [TestClu(params)]
+        self.clusterers: List[Clustering] = []
 
     def _load_data(self, param_name) -> Tuple[np.ndarray, int]:
         filename: str = self.params[param_name]
@@ -57,10 +46,15 @@ class Pipeline:
     def _extract_features(self, data: np.ndarray) -> pd.DataFrame:
         all_features = []
 
-        for example in data:
+        for i, example in enumerate(data):
             features = {}
             for feature_extractor in self.feature_extractors:
                 features.update(feature_extractor.extract_features(example))
+
+            # A None feature means that example is invalid
+            if None in features.values():
+                # Set all other features to None as well
+                features = dict.fromkeys(features, None)
             all_features.append(features)
 
         all_features = pd.DataFrame(all_features)
@@ -68,11 +62,16 @@ class Pipeline:
         return all_features
 
     def _predict(self, features):
+        def do_predict(series):
+            # Skip prediction for invalid examples
+            if series.isnull().any():
+                return DamageMode.INVALID
+            else:
+                return clusterer.predict(series.to_frame().transpose())
+
         predictions = {clusterer.name: None for clusterer in self.clusterers}
         for clusterer in self.clusterers:
-            predictions[clusterer.name] = features.apply(
-                lambda series: clusterer.predict(series.to_frame().transpose()), axis=1
-            )
+            predictions[clusterer.name] = features.apply(do_predict, axis=1)
 
         predictions = pd.concat(predictions, axis=1)
 
