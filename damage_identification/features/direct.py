c23625199478a4d72de80cb651f881d5206f2ce2
RolandvDijk <rolandvandijk1@gmail.com>
2022-03-11 12:09:28 +0100

    weldone

--- damage_identification/features/direct.py
+++ damage_identification/features/direct.py
@@ -34,7 +34,8 @@ class DirectFeatureExtractor(FeatureExtractor):
             params: parameters for the feature extractor, uses default parameters if None
         """
         if params is None:
-            params = {"feature_extractor_direct_threshold": 2.5e-2}
+            params = {"feature_extractor_direct_threshold": 2.5e-2, "feature_extractor_direct_n_sample": 200}
+
         super().__init__("direct", params)
 
     def extract_features(self, example: np.ndarray) -> Dict[str, float]:
@@ -49,6 +50,7 @@ class DirectFeatureExtractor(FeatureExtractor):
         """
         #counts
         threshold = self.params["feature_extractor_direct_threshold"]
+        nsample = self.params["feature_extractor_direct_n_sample"]
         above_threshold = example >= threshold
         count = 0
         iarray = np.array((0,0))
@@ -66,13 +68,17 @@ class DirectFeatureExtractor(FeatureExtractor):
         peakamplitudeindex = np.argmax(example)
         #rise time
         risetime = (peakamplitudeindex-iarray[0])*1/2048/1000 #in s
-        #energy
+        #energy (squared microvolt for 1/1000th second --> 10e-12V)
         timestamps = np.linspace(0, 1/1000, len(example)) #in s
-        cs = CubicSpline(timestamps, example)
+        cs = CubicSpline(timestamps, example*1000) #in microvolt!
         f = lambda x: cs(x)**2
         print(f(timestamps))
         energy = quad(f, timestamps[0], timestamps[-1], limit=100, epsabs=1e-10)
-        return {"peak_amplitude": peak_amplitude, "count": count, "duration": duration, "rise_time": risetime, "energy": energy}
+        #n-sample
+        new_dict = {"nsample_"+str(n+1): example[n] for n in range(nsample)}
+        return_dict = {"peak_amplitude": peak_amplitude, "count": count, "duration": duration, "rise_time": risetime, "energy": energy[0]}
+        final_dict = return_dict | new_dict
+        return final_dict
 
 results = DirectFeatureExtractor()
 
