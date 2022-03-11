4d42dfc83bca15fa9cd481196cd4ca2287738009
Dominik Stiller <domi.stiller@gmail.com>
2022-03-11 11:21:02 +0100

    Add CLI interface

--- damage_identification/cli.py
+++ damage_identification/cli.py
@@ -1,8 +1,30 @@
 """Parsing of command-line arguments"""
+import argparse
 from typing import Any, Dict
 
 from damage_identification.pipeline import PipelineMode
 
 
 def parse_cli_args() -> Dict[str, Any]:
-    return {"mode": PipelineMode.TRAINING, "training_data_file": "Waveforms.csv"}
+    parser = argparse.ArgumentParser(
+        prog="python -m damage_identification", description="Process some integers."
+    )
+    subparsers = parser.add_subparsers()
+
+    # Training mode
+    parser_training = subparsers.add_parser("train")
+    parser_training.set_defaults(mode=PipelineMode.TRAINING)
+    parser_training.add_argument("training_data_file", metavar="data_file")
+
+    # Prediction mode
+    parser_prediction = subparsers.add_parser("predict")
+    parser_prediction.set_defaults(mode=PipelineMode.PREDICTION)
+    parser_prediction.add_argument("prediction_data_file", metavar="data_file")
+
+    # Evaluation mode
+    parser_evaluation = subparsers.add_parser("evaluate")
+    parser_evaluation.set_defaults(mode=PipelineMode.EVALUATION)
+    parser_evaluation.add_argument("evaluation_data_file", metavar="data_file")
+
+    params = vars(parser.parse_args())
+    return params
--- damage_identification/pipeline.py
+++ damage_identification/pipeline.py
@@ -6,9 +6,21 @@ import numpy as np
 import pandas as pd
 
 from damage_identification.clustering.base import Clustering
+from damage_identification.damage_mode import DamageMode
 from damage_identification.features.base import FeatureExtractor
 from damage_identification.features.direct import DirectFeatureExtractor
 from damage_identification.io import load_uncompressed_data, load_compressed_data
+
+
+class TestClu(Clustering):
+    def __init__(self, params: Dict[str, Any]):
+        super(TestClu, self).__init__("testclusterer", params)
+
+    def train(self, examples: pd.DataFrame):
+        pass
+
+    def predict(self, example: pd.DataFrame) -> DamageMode:
+        return DamageMode.DELAMINATION
 
 
 class Pipeline:
@@ -17,22 +29,30 @@ class Pipeline:
     def __init__(self, params: Dict[str, Any]):
         self.params = params
         self.feature_extractors: List[FeatureExtractor] = [DirectFeatureExtractor(params)]
-        self.clusterers: List[Clustering] = []
+        self.clusterers: List[Clustering] = [TestClu(params)]
 
     def _load_data(self, param_name) -> Tuple[np.ndarray, int]:
         filename: str = self.params[param_name]
 
         if filename.endswith(".csv"):
             data = load_uncompressed_data(filename)
-        # TODO insert actual compressed file extension
-        elif filename.endswith("."):
+        elif filename.endswith(".tradb"):
             data = load_compressed_data(filename)
         else:
             raise Exception("Unsupported data file type")
 
-        n_rows = data.shape[0]
+        n_examples = data.shape[0]
 
-        return data, n_rows
+        return data, n_examples
+
+    def _load_pipeline(self):
+        for feature_extractor in self.feature_extractors:
+            feature_extractor.load(
+                os.path.join(self.PIPELINE_PERSISTENCE_FOLDER, feature_extractor.name)
+            )
+
+        for clusterer in self.clusterers:
+            clusterer.load(os.path.join(self.PIPELINE_PERSISTENCE_FOLDER, clusterer.name))
 
     def _extract_features(self, data: np.ndarray) -> pd.DataFrame:
         all_features = []
@@ -47,10 +67,22 @@ class Pipeline:
 
         return all_features
 
+    def _predict(self, features):
+        predictions = {clusterer.name: None for clusterer in self.clusterers}
+        for clusterer in self.clusterers:
+            predictions[clusterer.name] = features.apply(
+                lambda series: clusterer.predict(series.to_frame().transpose()), axis=1
+            )
+
+        predictions = pd.concat(predictions, axis=1)
+
+        return predictions
+
     def run_training(self):
         examples, n_examples = self._load_data("training_data_file")
+        print(f"Loaded training data set ({n_examples} examples)")
 
-        # TODO add filtering
+        # TODO run filtering
 
         # Train feature extractor and save model
         for feature_extractor in self.feature_extractors:
@@ -58,38 +90,43 @@ class Pipeline:
             feature_extractor.save(
                 os.path.join(self.PIPELINE_PERSISTENCE_FOLDER, feature_extractor.name)
             )
+        print("Trained feature extractors")
 
         # Extract features to training of PCA and features
-        examples_features = self._extract_features(examples)
+        features = self._extract_features(examples)
 
-        # TODO run PCA
+        # TODO run PCA training
 
         # Train clustering
         for clusterer in self.clusterers:
-            clusterer.train(examples_features)
+            clusterer.train(features)
             clusterer.save(os.path.join(self.PIPELINE_PERSISTENCE_FOLDER, clusterer.name))
+        print("Trained clusterers")
+
+        # Get predictions for training of cluster identification
+        predictions = self._predict(features)
+
+        # TODO run cluster identification training
 
     def run_prediction(self):
-        data, _ = self._load_data("prediction_data_file")
+        data, n_examples = self._load_data("prediction_data_file")
+        print(f"Loaded prediction data set ({n_examples} examples)")
 
-        # Load trained model
-        for feature_extractor in self.feature_extractors:
-            feature_extractor.load(
-                os.path.join(self.PIPELINE_PERSISTENCE_FOLDER, feature_extractor.name)
-            )
+        # TODO run filtering
 
-        data_features = self._extract_features(data)
+        features = self._extract_features(data)
+        print("Extracted features")
 
         # TODO run PCA
 
-        predictions = []
-        for clusterer in self.clusterers:
-            clusterer.load(os.path.join(self.PIPELINE_PERSISTENCE_FOLDER, clusterer.name))
-            # TODO change to iteration over single examples
-            predictions.append(clusterer.predict(data_features))
+        predictions = self._predict(features)
+        print("Predicted clusters")
+
+        # TODO run cluster identification
 
     def run_evaluation(self):
-        data, n_rows = self._load_data("evaluation_data_file")
+        data, n_examples = self._load_data("evaluation_data_file")
+        print(f"Loaded evaluation data set ({n_examples} examples)")
 
         # TODO add evaluation mode
 
