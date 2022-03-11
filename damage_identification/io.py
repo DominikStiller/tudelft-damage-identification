4ed99cb3b58f9c8ea8a2b34c222f446febe3935e
Dominik Stiller <domi.stiller@gmail.com>
2022-03-11 11:27:15 +0100

    Change file name handling

--- damage_identification/io.py
+++ damage_identification/io.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import vallenae as vae
 
+
 def load_uncompressed_data(filename: str) -> np.ndarray:
     """
     Args:
@@ -11,7 +12,7 @@ def load_uncompressed_data(filename: str) -> np.ndarray:
     Returns:
         Numpy array of the data in the data file
     """
-    data = np.transpose((pd.read_csv(os.path.join("data", filename))).to_numpy())
+    data = np.transpose((pd.read_csv(filename)).to_numpy())
     return data
 
 
@@ -23,6 +24,6 @@ def load_compressed_data(filename: str) -> np.ndarray:
     Returns:
         Numpy array of the data in the data file: n_examples x n_samples
     """
-    compressed_data = vae.io.TraDatabase(os.path.join("data", filename))
+    compressed_data = vae.io.TraDatabase(filename)
     data = np.vstack(compressed_data.read()["data"].to_numpy())
     return data
