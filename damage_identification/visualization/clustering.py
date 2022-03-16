d7de1675a3e30711f2b107bac2768ecb3cae8944
Dominik Stiller <domi.stiller@gmail.com>
2022-03-16 09:53:47 +0100

    Generate example data for clustering

--- damage_identification/visualization/clustering.py
+++ damage_identification/visualization/clustering.py
@@ -1,3 +1,31 @@
+from random import choice
+
 import numpy as np
 import matplotlib as plt
 import os
+
+import pandas as pd
+
+from damage_identification.damage_mode import DamageMode
+
+
+def generate_example_data():
+    n_examples = 50
+    n_pc = 3
+    features = pd.DataFrame(
+        np.random.rand(n_examples, n_pc), columns=[f"pca_{i+1}" for i in range(0, n_pc)]
+    )
+    predictions = pd.DataFrame(
+        [
+            choice(
+                [
+                    DamageMode.FIBER_PULLOUT,
+                    DamageMode.MATRIX_CRACKING,
+                    DamageMode.FIBER_MATRIX_DEBONDING,
+                ]
+            )
+            for _ in range(0, n_examples)
+        ],
+        columns=["mode_kmeans"],
+    )
+    return features, predictions
