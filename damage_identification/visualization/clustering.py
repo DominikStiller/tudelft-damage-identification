3d521a171d80ffab076ed3576a9c836cb809c92e
Bo <99323127+bolee02@users.noreply.github.com>
2022-03-16 12:10:53 +0100

    clean code

--- damage_identification/visualization/clustering.py
+++ damage_identification/visualization/clustering.py
@@ -1,40 +1,15 @@
-from random import choice
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from damage_identification.damage_mode import DamageMode
 
-
-def generate_example_data():
-    n_examples = 50
-    n_pc = 3
-    features = pd.DataFrame(
-        np.random.rand(n_examples, n_pc), columns=[f"pca_{i + 1}" for i in range(0, n_pc)]
-    )
-    predictions = pd.DataFrame(
-        [
-            choice(
-                [
-                    DamageMode.FIBER_PULLOUT,
-                    DamageMode.MATRIX_CRACKING,
-                    DamageMode.FIBER_MATRIX_DEBONDING,
-                ]
-            )
-            for _ in range(0, n_examples)
-        ],
-        columns=["mode_kmeans"],
-    )
-    return features, predictions
-
-
-class ClusteringVisualization():
+class ClusteringVisualization:
     """
     Performs visualization of clustered data based on a tuple of the first three primary components and their predicted damage modes
 
     Typical usage example:
         ClusteringVisualization().visualize_data()
     """
+
     def __init__(self, data: tuple):
         self.pca, self.mode = data
 
@@ -46,7 +21,8 @@ class ClusteringVisualization():
             dmg_mode: the requested damage mode to extract datapoints for.
 
         Returns:
-            a pandas dataframe that contains the three PCA values, for every entry of the requested damage mode.
+            classed_data: pandas dataframe that contains the three PCA values, for every entry of the requested
+            damage mode.
         """
 
         points = self.mode.loc[self.mode["mode_kmeans"] == dmg_mode]
@@ -55,35 +31,33 @@ class ClusteringVisualization():
 
     def visualize_kmeans(self):
         """
-        To visualize clustering from kmeans. Loops over all the damage modes present in the data, and plots the datapoints for each of these in a different
+        To visualize clustering from kmeans. Loops over all the damage modes present in the data, and plots the
+        datapoints for each of these in a different
         colour, in 3D space.
 
-        Args: -
+        Args:
+            self
 
-        Returns:-
+        Returns:
+            matplotlib visualization of the clusters
         """
 
-        ax = plt.axes(projection = "3d")
-        dmgmodes = self.mode["mode_kmeans"]
-        clusters = dmgmodes.drop_duplicates()
+        ax = plt.axes(projection="3d")
+        clusters = self.mode["mode_kmeans"].drop_duplicates()
         colour = ["b", "g", "r", "c", "m"]
         colourpicker = 0
         for cluster in clusters:
-            ax.scatter3D(self.classify_data(cluster)["pca_1"],
-                         self.classify_data(cluster)["pca_2"],
-                         self.classify_data(cluster)["pca_3"], c=colour[colourpicker])
+            ax.scatter3D(
+                self.classify_data(cluster)["pca_1"],
+                self.classify_data(cluster)["pca_2"],
+                self.classify_data(cluster)["pca_3"],
+                c=colour[colourpicker],
+            )
             colourpicker += 1
         ax.set_title("First three PCA directions")
         ax.set_xlabel("pca_1")
         ax.set_ylabel("pca_2")
         ax.set_zlabel("pca_3")
         plt.show()
-
-
-testdata = generate_example_data()
-ClusteringVisualization(testdata).visualize_kmeans()
-
-n_pc = 3
-n_examples = 6
 
 pcadata = pd.dataframe( ,columns=[f"pca_{i + 1}" for i in range(0, n_pc)])
