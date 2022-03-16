42c7a5371b524a961b85c625526a8f5e88d15aef
Bo <99323127+bolee02@users.noreply.github.com>
2022-03-16 10:59:02 +0100

    Changes by Luka-

--- damage_identification/visualization/clustering.py
+++ damage_identification/visualization/clustering.py
@@ -1,9 +1,8 @@
 from random import choice
-
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import os
-
+from mpl_toolkits.mplot3d import Axes3D
 import pandas as pd
 
 from damage_identification.damage_mode import DamageMode
@@ -29,3 +28,29 @@ def generate_example_data():
         columns=["mode_kmeans"],
     )
     return features, predictions
+
+def classify_data(dataset, dmg_mode):
+    features, predictions = dataset
+    points = predictions.loc[predictions["mode_kmeans"] == dmg_mode]
+    classed_data = pd.merge(features, points, left_index=True, right_index=True)
+    return classed_data
+
+def visualize_data(data):
+    fig = plt.figure()
+    ax = plt.axes(projection = "3d")
+    dmgmodes = data[1]["mode_kmeans"]
+    clusters = dmgmodes.drop_duplicates()
+    colour = ["b", "g", "r", "c", "m"]
+    colourpicker = 0
+    for cluster in clusters:
+        ax.scatter3D(classify_data(data, cluster)["pca_1"],
+                     classify_data(data, cluster)["pca_2"],
+                     classify_data(data, cluster)["pca_3"], c=colour[colourpicker])
+        colourpicker += 1
+    ax.set_title("First three PCA directions")
+    ax.set_xlabel("pca_1")
+    ax.set_ylabel("pca_2")
+    ax.set_zlabel("pca_3")
+    plt.show()
+
+visualize_data(testdata)
