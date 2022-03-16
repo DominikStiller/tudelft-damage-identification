19a5bf955d2f78cb6af5d4a8a4c44f18fb1694af
Dominik Stiller <domi.stiller@gmail.com>
2022-03-16 10:37:18 +0100

    Add load/save to PCA

--- damage_identification/pca.py
+++ damage_identification/pca.py
@@ -2,7 +2,7 @@ from sklearn.decomposition import PCA
 import numpy as np
 
 
-class PricipalComponents():
+class PricipalComponents:
     def __init__(self):
         """
         Initialize the PCA dimensionality reductor.
@@ -25,40 +25,40 @@ class PricipalComponents():
 
     def load(self, directory):
         """
-        Loads the state of the PCA.
+                Loads the state of the PCA.
 
-        This method should only load files from the directory specified in the argument.
-
-        Args:
-            directory: the directory to load the state from
+                This method should only load files from the directory specified in the argument.
+        z
+                Args:
+                    directory: the directory to load the state from
         """
         pass
 
-    def transform(self, data: np.ndarray):
+    def transform(self, data: np.ndarray) -> np.ndarray:
         """
         Reduce the feature dimension of an example based on principal components identified in train().
 
         Args:
             data: the single example (shape 1 x n_features)
+        Returns:
+            a NumPy array (shape 1 x n_features_reduced)
         """
 
         pass
 
-
-    def train(self, data: np.ndarray, explained_variance: float) -> np.ndarray:
+    def train(self, data: np.ndarray, explained_variance: float):
         """
         Identify the principal components.
 
         Args:
             data: all data for training (shape n_examples x n_features)
             explained_variance: the desired explained variance to select the number of principal components to return (i.e. n_features_reduced)
-        Returns:
-           a NumPy array (shape n_examples x n_features_reduced)
         """
         pca = PCA(n_components=explained_variance)
         pca.fit(data)
         reduced = pca.transform(data)
         return reduced
+
 
 principal = PricipalComponents("PCA")
 X = np.random.rand(5000, 2000)
