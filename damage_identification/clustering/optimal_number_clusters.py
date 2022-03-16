eb8ff70cab0659ed37bf3c08123a7c6d405d38fc
MihaiFetecau <99323165+MihaiFetecau@users.noreply.github.com>
2022-03-16 12:21:25 +0100

    Update optimal_number_clusters.py

--- damage_identification/clustering/optimal_number_clusters.py
+++ damage_identification/clustering/optimal_number_clusters.py
@@ -1,7 +1,12 @@
-
+from typing import Dict
 import pandas as pd
+import validclust as vld
+from sklearn.datasets import make_blobs
+import numpy as np
+import matplotlib as plt
 
-def find_optimal_number_of_clusters(features: pd.DataFrame, n_start, n_end) -> int:
+
+def find_optimal_number_of_clusters(features: pd.DataFrame, n_start, n_end) -> Dict[str, float]:
     """
     Find the optimal number of clusters based on a voting scheme of Davies-Bouldin, Silhouette and Dunn indexes.
 
@@ -13,5 +18,35 @@ def find_optimal_number_of_clusters(features: pd.DataFrame, n_start, n_end) -> i
         Optimal number of clusters
     """
     # Call validclust
+
+    vclust = vld.ValidClust(k = list(range(n_start, n_end+1)), methods= ['kmeans', 'hierarchical'])
+    cvi_vals = vclust.fit_predict(features)
+    indices = cvi_vals.to_numpy()
+
     # Compare all results and find the single optimal number
-    pass
+    maximize = [0, 1, 3, 4, 5, 7]
+    minimize = [2, 6]
+    #Normalize indices that need to be maximized
+    for i in maximize:
+        maximum = np.max(indices[i][:])
+        minimum = np.min(indices[i][:])
+        for j in range(n_end-n_start+1):
+            indices[i][j] = (indices[i][j] - minimum)/(maximum-minimum)
+    #Normalize indices that need to be minimized
+
+    for i in minimize:
+        minimum = np.min(indices[i][:])
+        maximum = np.max(indices[i][:])
+        for j in range(n_end-n_start+1):
+            indices[i][j] = (indices[i][j]-maximum)/(minimum-maximum)
+    
+
+
+
+
+
+    return
+
+
+dummy_data, _ = make_blobs(n_samples=100, centers=4, n_features=30, random_state=0)
+find_optimal_number_of_clusters(dummy_data, 3, 6)
