681d9ee35be4583b19cf82399374165a48672c11
ja-key <76249996+ja-key@users.noreply.github.com>
2022-03-18 09:59:39 +0100

    removed saving and loading test

--- tests/test_pca.py
+++ tests/test_pca.py
@@ -1,18 +1,12 @@
 from unittest import TestCase
 import numpy as np
 from damage_identification.pca import PrincipalComponents
-import os
-
-os.chdir("C:/Users/jakub/Desktop/Test")
-
 
 class TestPCA(TestCase):
     def test_pca(self):
         example = np.array([[1, 1, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 1, 0, 0]])
         model = PrincipalComponents({"explained_variance": 0.9})
         model.train(example)
-        model.save("")
-        model.load("")
         result = model.transform(example)
         # print(len(result[0]))
         # Expected value: 1000 Hz
