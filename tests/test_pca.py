1cda0c43236ec6b4ac71cada89f3289d5e862d63
ja-key <76249996+ja-key@users.noreply.github.com>
2022-03-18 10:01:38 +0100

    removed saving and loading test

--- tests/test_pca.py
+++ tests/test_pca.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 import numpy as np
 from damage_identification.pca import PrincipalComponents
 
+
 class TestPCA(TestCase):
     def test_pca(self):
         example = np.array([[1, 1, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 1, 0, 0]])
