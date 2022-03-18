4e2d95a32fd88e0f78b8ec78b052c9e0943b84db
Jessedc2001 <jessedelcanho@gmail.com>
2022-03-18 11:17:27 +0100

    pca classs framework

---
+++ damage_identification/clustering/fuzzy_c_means.py
@@ -0,0 +1,19 @@
+import os
+import numpy as np
+import skfuzzy as fuzz
+import pickle
+
+class FuzzycmeansClusterer(Clusterer):
+
+    def __init__(self, params: Dict[str, Any]):
+
+
+        super(KmeansClusterer, self).__init__("kmeans", params)
+
+    def save
+
+    def load
+
+    def Gen_Functions
+
+    def Predict
