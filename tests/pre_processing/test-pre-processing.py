7d2fe368446c986477c4d1cbefd6034166c8cbf7
tompson-07 <79365270+tomasteixeirapijpers@users.noreply.github.com>
2022-04-20 11:17:32 +0200

    Add inspection method for wavelet filtering
    TODO: only run directly

---
+++ tests/pre_processing/test-pre-processing.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+import numpy as np
+
+from damage_identification.preprocessing.wavelet_filtering import WaveletFiltering
+from damage_identification.io import load_uncompressed_data, load_compressed_data
+
+
+class TestWaveletFiltering(TestCase):
+    # def test_wavelet_filtering(self):
+    #     wave_object = WaveletFiltering({"wavelet_family": "db", "wavelet_scale": 3})
+    #     wave_object.load_data("data/Waveforms.csv")
+    #     wave_object.filtering(wave_object.waveform, "optimal")
+
+    def test_signal_prep(self):
+        filter = WaveletFiltering({"wavelet_family": "db", "wavelet_scale": 3})
+        raw = load_compressed_data("data/comp0.tradb")
+        filtered = filter.filter(raw)
+        # np.savetxt("sample.csv", filtered[6722, :], delimiter=",")
+        filter.wavelet_plot(raw, filtered, 6722)
