3ee9cf9244a71b2b164737a4da7b67be43cfca20
tompson-07 <79365270+tomasteixeirapijpers@users.noreply.github.com>
2022-04-20 11:26:42 +0200

    Add execute only locally
    only executes when running file directly
    formatted w/ black

--- tests/pre_processing/test-pre-processing.py
+++ tests/pre_processing/test-pre-processing.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 import numpy as np
+import sys
 
 from damage_identification.preprocessing.wavelet_filtering import WaveletFiltering
 from damage_identification.io import load_uncompressed_data, load_compressed_data
@@ -13,8 +14,9 @@ class TestWaveletFiltering(TestCase):
     #     wave_object.filtering(wave_object.waveform, "optimal")
 
     def test_signal_prep(self):
-        filter = WaveletFiltering({"wavelet_family": "db", "wavelet_scale": 3})
-        raw = load_compressed_data("data/comp0.tradb")
-        filtered = filter.filter(raw)
-        # np.savetxt("sample.csv", filtered[6722, :], delimiter=",")
-        filter.wavelet_plot(raw, filtered, 6722)
+        if __name__ == "__main__":
+            filter = WaveletFiltering({"wavelet_family": "db", "wavelet_scale": 3})
+            raw = load_compressed_data("data/comp0.tradb")
+            filtered = filter.filter(raw)
+            # np.savetxt("sample.csv", filtered[6722, :], delimiter=",")
+            filter.wavelet_plot(raw, filtered, 6722)
