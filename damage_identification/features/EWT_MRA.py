552c542f2dd7312e04b88f91e52055647012c1e4
tompson-07 <79365270+tomasteixeirapijpers@users.noreply.github.com>
2022-04-29 16:43:29 +0200

    Add exception handling
    Add exception handling for incompatible configuration
    Executed Black formatting

--- damage_identification/features/EWT_MRA.py
+++ damage_identification/features/EWT_MRA.py
@@ -1,5 +1,6 @@
 import pywt
 from damage_identification import io
+import warnings
 
 
 class MultiResolutionAnalysis:
@@ -9,19 +10,28 @@ class MultiResolutionAnalysis:
     """
 
     # def __init__(self, params: Dict[str: Any]):
-    def __init__(self, wavelet, mode, time_bands, dec_level):
+    def __init__(self, wave_fam, wave_mag, time_bands, dec_level):
         """
         Initializes object with specifications for decomposing signal.
 
         Parameters:
             - wavelet: the name and magnitude of the wavelet family (e.g.: 'db38' or 'coif17')
-            - mode: the decomposition mode used (e.g.: 'symmetric' or )
             - time_bands: the amount of time bands to split the energy information (i.e. with time_bands = 4, the energy information describes the 4 quarters of the signal)
             - dec_level: the decomposition level of the signal (i.e. the amount of carrier frequencies the signal will be split up into. e.g.: decomposition level of 4 gives 2^4 = 16 frequencies)
         """
+
         self.signal_data = []
-        self.wavelet = wavelet
-        self.mode = mode
+        if wave_fam == "db" or wave_fam == "coif":
+            if wave_fam == "db" and 1 < wave_mag <= 38:
+                self.wavelet = wave_fam + str(wave_mag)
+            elif wave_fam == "coif" and 1 <= wave_mag <= 17:
+                self.wavelet = wave_fam + str(wave_mag)
+            else:
+                raise ValueError("Magnitude not compatible.")
+        else:
+            raise ValueError("Wavelet not compatible.")
+
+        self.mode = "symmetric"
         self.time_bands = time_bands
         self.dec_level = dec_level
         self.wave_coeffs = []
@@ -57,8 +67,15 @@ class MultiResolutionAnalysis:
             - None
         """
         wp = pywt.WaveletPacket(data=self.signal_data, wavelet=self.wavelet, mode=self.mode)
-        for level in range(1, wp.maxlevel+1):
-            self.wave_coeffs.append([wp[node.path].data for node in wp.get_level(level, 'natural')])
+
+        if wp.maxlevel < self.dec_level:
+            self.dec_level = wp.maxlevel
+            warnings.warn(
+                "Decomposition level is greater than the maximum allowed level. Consider choosing a lower decomposition level."
+            )
+
+        for level in range(1, self.dec_level + 1):
+            self.wave_coeffs.append([wp[node.path].data for node in wp.get_level(level, "natural")])
 
         return self.wave_coeffs
 
@@ -72,17 +89,26 @@ class MultiResolutionAnalysis:
         energy = []
         time_energy = []
         c = 0
-        tot_energy = sum(self.wave_coeffs[0][0][:]**2)+sum(self.wave_coeffs[0][1][:]**2)
-        for i in range(0, 2**self.dec_level):
-            # print(len(coeffs[self.dec_level-1][i]))
-            # print(coeffs[self.dec_level-1][i][:])
-            for t in range(0, self.time_bands):
-                left_t_bound = int((t/self.time_bands)*len(self.wave_coeffs[self.dec_level-1][i][:]))
-                # print(i,t)
-                # print(coeffs[self.dec_level-1][i][:])
-                right_t_bound = int((((t+1)/self.time_bands)*len(self.wave_coeffs[self.dec_level-1][i][:])))
-                time_energy.append(sum(self.wave_coeffs[(self.dec_level-1)][i][left_t_bound:right_t_bound]**2)/tot_energy)
+        tot_energy = sum(self.wave_coeffs[0][0][:] ** 2) + sum(self.wave_coeffs[0][1][:] ** 2)
 
+        if len(self.wave_coeffs[self.dec_level - 1][0][:]) < self.time_bands:
+            self.time_bands = len(self.wave_coeffs[self.dec_level - 1][0][:])
+            warnings.warn(
+                "The time band resolution is too high for this decomposition level. Defaulted to maximum allowable resolution (frequency energy is for entire wave). Consider choosing a lower time resolution."
+            )
+
+        for i in range(0, 2**self.dec_level):
+            for t in range(0, self.time_bands):
+                left_t_bound = int(
+                    (t / self.time_bands) * len(self.wave_coeffs[self.dec_level - 1][i][:])
+                )
+                right_t_bound = int(
+                    (((t + 1) / self.time_bands) * len(self.wave_coeffs[self.dec_level - 1][i][:]))
+                )
+                time_energy.append(
+                    sum(self.wave_coeffs[(self.dec_level - 1)][i][left_t_bound:right_t_bound] ** 2)
+                    / tot_energy
+                )
             energy.append(time_energy)
             time_energy = []
 
