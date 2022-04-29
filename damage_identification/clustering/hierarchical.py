71dd8b17ae2a5d74337ac16ba7983dffe9cbccb4
ja-key <76249996+ja-key@users.noreply.github.com>
2022-04-29 17:00:44 +0200

    Changes by bolee

--- damage_identification/clustering/hierarchical.py
+++ damage_identification/clustering/hierarchical.py
@@ -98,16 +98,13 @@ class HierarchicalClusterer(Clusterer):
 
 
 def plot_dendrogram(model, **kwargs):
-    # Create linkage matrix and then plot the dendrogram
-
-    # create the counts of samples under each node
     counts = np.zeros(model.children_.shape[0])
     n_samples = len(model.labels_)
     for i, merge in enumerate(model.children_):
         current_count = 0
         for child_idx in merge:
             if child_idx < n_samples:
-                current_count += 1  # leaf node
+                current_count += 1
             else:
                 current_count += counts[child_idx - n_samples]
         counts[i] = current_count
@@ -115,8 +112,6 @@ def plot_dendrogram(model, **kwargs):
     linkage_matrix = np.column_stack(
         [model.children_, model.distances_, counts]
     ).astype(float)
-
-    # Plot the corresponding dendrogram
     dendrogram(linkage_matrix, **kwargs)
 
 
