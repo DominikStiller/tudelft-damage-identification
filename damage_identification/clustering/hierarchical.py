56232f2228e3c41182e2f42134729ffa67992fe4
Luka140 <luka-groot@hotmail.nl>
2022-04-29 14:30:53 +0200

    Hierarchical test

--- damage_identification/clustering/hierarchical.py
+++ damage_identification/clustering/hierarchical.py
@@ -2,12 +2,13 @@ import numpy as np
 import os
 import pickle
 from typing import Dict, Any
-
-from sklearn.cluster import AgglomerativeClustering
+import pandas as pd
+from sklearn.cluster import AgglomerativeClustering, KMeans
 from sklearn.neighbors import KNeighborsClassifier
-
 from damage_identification.clustering.base import Clusterer
 
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import dendrogram
 
 class HierarchicalClusterer(Clusterer):
     """
@@ -59,10 +60,16 @@ class HierarchicalClusterer(Clusterer):
         Args:
             testdata: data used to create the clusters to train the hclust model
         """
+        #KMeansmodel = KMeans(self.params["n_clusters"], random_state=0)
+        #KMeansmodel = KMeansmodel.fit(testdata)
+        '''
         clusterer = AgglomerativeClustering(n_clusters=self.params["n_clusters"], linkage="ward")
         labeleddata = clusterer.fit_predict(testdata)
         self.model = KNeighborsClassifier(n_neighbors=self.params["n_neighbors"])
         self.model.fit(testdata, labeleddata)
+        '''
+        self.model = AgglomerativeClustering(n_clusters=self.params["n_clusters"], linkage="ward", compute_distances=True)
+        self.model.fit(testdata)
         return self.model
 
     def predict(self, data) -> int:
@@ -72,5 +79,50 @@ class HierarchicalClusterer(Clusterer):
         Args:
             data: datapoint for which the label should be predicted using the created hierarchical clustering model
         """
-        prediction = self.model.predict(data)
+        prediction = self.model.fit_predict(data)
         return prediction
+
+
+def plot_dendrogram(model, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
+
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack(
+        [model.children_, model.distances_, counts]
+    ).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
+
+
+
+data = pd.read_pickle('data\pca.pickle')
+#print(data)
+#clustering = AgglomerativeClustering().fit(data)
+#print(clustering.fit_predict(data))
+
+
+hc = HierarchicalClusterer({"n_clusters": 3, "n_neighbors": 5})
+hct = hc.train(data)
+#hcp = hc.predict(data)
+
+
+plt.title("Hierarchical Clustering Dendrogram")
+# plot the top three levels of the dendrogram
+plot_dendrogram(hct, truncate_mode="level", p=5)
+plt.xlabel("Number of points in node (or index of point if no parenthesis).")
+plt.show().
+
+
+
