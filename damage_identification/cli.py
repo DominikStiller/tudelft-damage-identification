f5012461c0314380b37a1d8b5d841450894071db
Dominik Stiller <domi.stiller@gmail.com>
2022-04-29 15:25:48 +0200

    Fix default value of sampling rate

--- damage_identification/cli.py
+++ damage_identification/cli.py
@@ -20,9 +20,12 @@ def parse_cli_args() -> dict[str, Any]:
         else:
             raise "Invalid value for n_clusters"
 
-    # Fix skip_filter not being stored if absent even though store_true should handle this
+    # Default params for argparse are not working for some reason
+    # Therefore, set defaults manually here
     if "skip_filter" not in params:
         params["skip_filter"] = False
+    if "sampling_rate" not in params:
+        params["sampling_rate"] = 1000 * 2048  # 2048 samples per ms
 
     return params
 
@@ -53,9 +56,7 @@ def _construct_parser() -> ArgumentParser:
     )
     parser_training.set_defaults(mode=PipelineMode.TRAINING)
 
-    parser_training.add_argument(
-        "--sampling_rate", type=float, default=1000 * 2048
-    )  # 2048 samples per ms
+    parser_training.add_argument("--sampling_rate", type=float)
     parser_training.add_argument("--wavelet_family", type=str)
     parser_training.add_argument("--wavelet_scale", type=int)
     parser_training.add_argument("--wavelet_threshold", type=str)
