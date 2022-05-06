50512c07cb330df76414439ab56a3749a784a2df
Dominik Stiller <domi.stiller@gmail.com>
2022-05-06 16:51:00 +0200

    Add standalone script for peak splitting

--- damage_identification/preprocessing/peak_splitter.py
+++ damage_identification/preprocessing/peak_splitter.py
@@ -1,10 +1,5 @@
-import sys
-import time
-
 import numpy as np
 from tqdm import tqdm
-
-from damage_identification.io import load_uncompressed_data
 
 
 class PeakSplitter:
@@ -113,3 +108,34 @@ class PeakSplitter:
                 pbar.update()
 
         return np.vstack(examples), n_no_peaks, n_one_peak, n_over_two_peaks
+
+
+if __name__ == "__main__":
+    import os
+    import sys
+
+    from damage_identification.io import load_uncompressed_data, load_compressed_data
+
+    filename = sys.argv[1]
+    filename_split = os.path.splitext(filename)[0] + ".npy"
+
+    print("Loading data...")
+    if filename.endswith(".csv"):
+        data = load_uncompressed_data(filename)
+    elif filename.endswith(".tradb"):
+        data = load_compressed_data(filename)
+    else:
+        raise Exception("Unsupported data file type")
+
+    data = data[:10]
+
+    print("Splitting data...")
+    data_split, n_no_peaks, n_one_peak, n_over_two_peaks = PeakSplitter.split_all(data)
+
+    print(f"Dataset with originally {data.shape[0]} contains")
+    print(f" - {n_no_peaks} examples without peaks")
+    print(f" - {n_one_peak} examples with one peak peaks")
+    print(f" - {n_over_two_peaks} examples with two peaks or more")
+
+    np.save(filename_split, data_split)
+    print(f"Saved split data of {data_split.shape[0]} examples to {filename_split}")
