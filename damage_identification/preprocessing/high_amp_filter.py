0d08298c72e37620489a89a9c9dba9a24cd0b4d0
JoseBCunha <josebcunha2002@gmail.com>
2022-05-06 16:37:50 +0200

    Update high_amp_filter.py

--- damage_identification/preprocessing/high_amp_filter.py
+++ damage_identification/preprocessing/high_amp_filter.py
@@ -3,23 +3,28 @@ import numpy as np
 
 from damage_identification.io import load_compressed_data
 
-class HighAmpFiltering():
+
+class HighAmpFiltering:
+    """
+    This class preprocesses the data by removing all signals for which the amplitude is greater tha 0.0995 -  caused by the sensor being cut off at values greater than that
+    """
 
     def __init__(self):
         return
 
-
     def filter_signal(self, data):
+        """
+        Filters all waveforms as previously described
+        """
         signal = data
         idx = []
-        for i in range(0, len(signal)-1):
+        for i in range(0, len(signal) - 1):
             x = max(signal[i, :])
             if x > 0.0995:
                 idx.append(i)
         for j in idx:
             data = np.delete(data, j, axis=0)
         return data
-
 
     def plot(self, data, n):
         plt.figure(figsize=(10, 6))
@@ -31,4 +36,4 @@ if __name__ == "__main__":
     filter = HighAmpFiltering()
     signal = load_compressed_data("data/comp0.tradb")
     new_signal = filter.filter_signal(signal)
-    filter.plot(new_signal,555)
+    filter.plot(new_signal, 555)
