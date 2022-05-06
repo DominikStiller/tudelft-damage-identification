8e5ff77de82be6f529b2d10d355351294ec43d88
JoseBCunha <josebcunha2002@gmail.com>
2022-05-06 16:21:43 +0200

    remove all signals with amplitude above certain threshold

---
+++ damage_identification/preprocessing/high_amp_filter.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from damage_identification.io import load_compressed_data
+
+class HighAmpFiltering():
+
+    def __init__(self):
+        self.threshold = 0.097
+        return
+
+
+    def filter_signal(self, data):
+        signal = data
+        idx = []
+        for i in range(0, len(signal)-1):
+            x = max(signal[i, :])
+            if x > self.threshold:
+                idx.append(i)
+        for j in idx:
+            data = np.delete(data, j, axis=0)
+        return data
+
+
+    def plot(self, data, n):
+        plt.figure(figsize=(10, 6))
+        plt.plot(data[n, :], label="Raw", color="b")
+        plt.show()
+
+
+if __name__ == "__main__":
+    filter = HighAmpFiltering()
+    signal = load_compressed_data("data/comp0.tradb")
+    new_signal = filter.filter_signal(signal)
+    filter.plot(new_signal,555)
