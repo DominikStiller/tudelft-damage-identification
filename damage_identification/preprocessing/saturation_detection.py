b14a3c6418230d69eb3d49b9e8e7465346bf7352
Dominik Stiller <domi.stiller@gmail.com>
2022-05-06 17:17:18 +0200

    Use vectorization for saturation detection

---
+++ damage_identification/preprocessing/saturation_detection.py
@@ -0,0 +1,21 @@
+from typing import Any
+
+import numpy as np
+
+
+class SaturationDetection:
+    """
+    This class preprocesses the data by removing all signals for which the amplitude is greater than 0.0995,
+    caused by the sensor being cut off at values greater than that due to saturation.
+    """
+
+    def __init__(self, params: dict[str, Any] = None):
+        if "saturation_threshold" not in self:
+            params["saturation_threshold"] = 0.0995
+        self.params = params
+
+    def filter_signal(self, data):
+        """
+        Filters all waveforms as previously described
+        """
+        return data[np.where(np.amax(data, axis=1) <= self.params["saturation_threshold"])]
