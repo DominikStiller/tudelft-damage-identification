78416ef8f4ad6877b2f4012ca07993e11808db37
Luka140 <luka-groot@hotmail.nl>
2022-05-08 16:50:16 +0200

    Fixed issues with differently formatted files

--- damage_identification/evaluation/force_displacement_visualization.py
+++ damage_identification/evaluation/force_displacement_visualization.py
@@ -9,20 +9,24 @@ def force_displacement(filename):
     plots the force against the displacement
     '''
 
-    data = pd.read_csv(f"../../data/{filename}", delimiter='\t', encoding='utf-8', header=2)
-    data = data.drop(index = 0)
+    data = pd.read_csv(f"../../data/{filename}", delimiter='\t', encoding='utf-8', header=2, error_bad_lines=False)
+    data = data.drop(index = data.index[data['Time'].str.isalpha()])
 
-    data = data.rename(columns={'250 kN MTS Force':'force', 'Running Time':'runtime', 'Time':'time', '250 kN MTS Displacement':'displacement'})
+    colreplacement = {'.* kN MTS Force':'force', 'Running Time':'runtime', 'Time':'time', '.* MTS Displacement':'displacement'}
+    data.columns = data.columns.to_series().replace(colreplacement, regex=True)
+
     for column in data.columns:
         data[column] = [x.replace(',', '.') for x in data[column]]
         data[column] = data[column].astype(float)
-    data['force'] = -data['force']
-    data['displacement'] = -data['displacement']
+
+    data['force'], data['displacement'] = -data['force'], -data['displacement']
+
     pl.plot(data['displacement'], data['force'])
     pl.xlabel('Displacement [mm]')
     pl.ylabel('Force [kN]')
-    pl.title()
+    pl.title('Placeholder')
     pl.show()
+    #pl.savefig()
+    print('done')
 
-
-force_displacement("specimen_comp90.dat")
+force_displacement("specimen.dat")
