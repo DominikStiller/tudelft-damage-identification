fixed improper normalization

damage_identification/evaluation/cluster_performace.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import davies_bouldin_score, silhouette_score
+from sklearn.metrics import davies_bouldin_score, silhouette_score, calinski_harabasz_score
 import pickle
 import os
 import pandas as pd
@@ -28,7 +28,8 @@ def get_metrics(data, labels, distmatrix):
     #distmatrix = fastdist.matrix_pairwise_distance(data.to_numpy(), fastdist.euclidean, "euclidean", return_matrix=True)
     #distmatrix = FCM._dist(data, data)
     dunnmetric = dunn(distmatrix, labels)
-    return [davies, silhouette, dunnmetric]
+    calinski = calinski_harabasz_score(data, labels)
+    return [davies, silhouette, dunnmetric, calinski]
 
 def collate_metrics(data, directory , indices):
     """
@@ -48,6 +49,5 @@ def collate_metrics(data, directory , indices):
     h_labels = load_labels(os.path.join(directory, "hierarchical/hclust.pickle"), indices)
     h_metrics = np.array(get_metrics(data, h_labels, distmatrix))
     collated = np.vstack((k_metrics, f_metrics, h_metrics))
-    collated[:, 0] = np.abs((collated[:, 0] - np.abs(np.max(collated[:, 0])))/np.max(collated[:, 0]))
-    collated[:, 1] = (collated[:, 1] + 1) * 0.5
-    return pd.DataFrame(collated, columns=['Davies', 'Silhouette', 'Dunn'], index=['kmeans', 'fcmeans', "hierarchical"])
+
+    return pd.DataFrame(collated, columns=['Davies', 'Silhouette', 'Dunn', 'Calinski-Harabasz'], index=['kmeans', 'fcmeans', "hierarchical"])

damage_identification/evaluation/cluster_performance_graphs.py
@@ -12,21 +12,29 @@ def graph_metrics(directory):
     fig = plt.figure()
     minclusters = 2
     x = range(minclusters, len(dirs) + minclusters)
-    k_metrics = np.zeros((len(dirs), 3))
-    f_metrics = np.zeros((len(dirs), 3))
-    h_metrics = np.zeros((len(dirs), 3))
+    k_metrics = np.zeros((len(dirs), 4))
+    f_metrics = np.zeros((len(dirs), 4))
+    h_metrics = np.zeros((len(dirs), 4))
     for i, d in enumerate(dirs):
         print(d)
-        data = pd.read_pickle(os.path.join(d, "training_features_pca.pickle.bz2")).sample(n = 10000)
+        data = pd.read_pickle(os.path.join(d, "training_features_pca.pickle.bz2")).sample(n = 30000)
         indices = data.index
         metrics = collate_metrics(data, d, indices).to_numpy()
+        print(metrics)
         k_metrics[i] = metrics[0]
         f_metrics[i] = metrics[1]
         h_metrics[i] = metrics[2]
 
-    labels = ["Davies-Bouldin", "Silhouette", "Dunn"]
-    for i in range(3):
-       plt.scatter(x, k_metrics[:, i], label=labels[i])
+
+    for i in [k_metrics, f_metrics, h_metrics]:
+        i[:, 0] = np.abs((i[:, 0] - np.abs(np.max(i[:, 0]))) / np.max(i[:, 0]))
+        i[:, 1] = (i[:, 1] + 1) * 0.5
+        i[:, 3] = np.abs(
+            (i[:, 3] - np.min(i[:, 3])) / (np.max(i[:, 3]) - np.min(i[:, 3])))
+
+    labels = ["Davies-Bouldin", "Silhouette", "Dunn", "Calinski-Harabasz"]
+    for i in range(len(labels)):
+       plt.plot(x, k_metrics[:, i], label=labels[i], marker='o')
 
     plt.xticks(x)
     plt.legend(loc=6)
@@ -35,8 +43,8 @@ def graph_metrics(directory):
     plt.savefig("kmeans.png")
     plt.clf()
 
-    for i in range(3):
-        plt.scatter(x, f_metrics[:, i], label=labels[i])
+    for i in range(len(labels)):
+        plt.plot(x, f_metrics[:, i], label=labels[i], marker='o')
     plt.xticks(x)
     plt.legend()
     plt.ylabel("Index score")
@@ -44,8 +52,8 @@ def graph_metrics(directory):
     plt.savefig("fcmeans.png")
     plt.clf()
 
-    for i in range(3):
-        plt.scatter(x, h_metrics[:, i], label=labels[i])
+    for i in range(len(labels)):
+        plt.plot(x, h_metrics[:, i], label=labels[i], marker='o')
     plt.xticks(x)
     plt.legend()
     plt.ylabel("Index score")