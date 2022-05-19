d007b40ca839e041042da03def33506284fa0f31
Luka140 <luka-groot@hotmail.nl>
2022-05-19 15:11:51 +0200

    Changed to fit in the pipeline better

--- damage_identification/evaluation/force_displacement_visualization.py
+++ damage_identification/evaluation/force_displacement_visualization.py
@@ -3,28 +3,11 @@ import matplotlib.pyplot as plt
 from damage_identification.evaluation.plot_helpers import save_plot, format_plot_2d_linear
 
 
-def force_displacement(filename, results_folder: str):
-    data = pd.read_csv(
-        f"../../data/{filename}", delimiter="\t", encoding="utf-8", header=2, on_bad_lines="skip"
-    )
-    data = data.drop(index=data.index[data["Time"].str.isalpha()])
-    colreplacement = {
-        ".* kN MTS Force": "force",
-        "Running Time": "runtime",
-        "Time": "time",
-        ".* MTS Displacement": "displacement",
-    }
-    data.columns = data.columns.to_series().replace(colreplacement, regex=True)
-
-    for column in data.columns:
-        data[column] = [x.replace(",", ".") for x in data[column]]
-        data[column] = data[column].astype(float)
-
-    data["force"], data["displacement"] = -data["force"], -data["displacement"]
+def force_displacement(data: pd.DataFrame, filename: str, results_folder: str):
 
     plt.plot(data["displacement"], data["force"])
     plt.xlabel("Displacement [mm]")
     plt.ylabel("Force [kN]")
 
     format_plot_2d_linear()
-    save_plot(results_folder, name=f"force_displacement_{filename}", fig=plt)
+    save_plot(results_folder=results_folder, name=f"force_displacement_{filename}", fig=plt)
