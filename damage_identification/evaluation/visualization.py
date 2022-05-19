a3d4341b36179cce12abef5566edd1284299aa8f
Luka140 <luka-groot@hotmail.nl>
2022-05-19 15:31:10 +0200

    Moved the force displacement graphs into the visualization file

--- damage_identification/evaluation/visualization.py
+++ damage_identification/evaluation/visualization.py
@@ -2,7 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-from damage_identification.evaluation.plot_helpers import save_plot, format_plot_3d, format_plot_2d
+from damage_identification.evaluation.plot_helpers import (
+    save_plot,
+    format_plot_3d,
+    format_plot_2d,
+    format_plot_2d_linear,
+)
 
 
 def visualize_clusters(data: pd.DataFrame, clusterer_names: list[str], results_folder: str):
@@ -78,3 +83,13 @@ def visualize_cumulative_energy(
 
             format_plot_2d()
             save_plot(results_folder, f"energy_plot_{clusterer}_{current_cluster}", plt)
+
+
+def force_displacement(data: pd.DataFrame, filename: str, results_folder: str):
+
+    plt.plot(data["displacement"], data["force"])
+    plt.xlabel("Displacement [mm]")
+    plt.ylabel("Force [kN]")
+
+    format_plot_2d_linear()
+    save_plot(results_folder=results_folder, name=f"force_displacement_{filename}", fig=plt)
