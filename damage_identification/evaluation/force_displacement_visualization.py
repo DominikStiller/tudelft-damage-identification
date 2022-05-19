7557eaeabcd9e59f638e66265a5e99f142d50b93
Luka140 <luka-groot@hotmail.nl>
2022-05-19 15:04:23 +0200

    Updated force_displacement graphs
    Also added a plot formatter for non-log plots to plot helpers

--- damage_identification/evaluation/force_displacement_visualization.py
+++ damage_identification/evaluation/force_displacement_visualization.py
@@ -1,32 +1,30 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as pl
+import matplotlib.pyplot as plt
+from damage_identification.evaluation.plot_helpers import save_plot, format_plot_2d_linear
 
 
-
-def force_displacement(filename):
-    '''
-    plots the force against the displacement
-    '''
-
-    data = pd.read_csv(f"../../data/{filename}", delimiter='\t', encoding='utf-8', header=2, error_bad_lines=False)
-    data = data.drop(index = data.index[data['Time'].str.isalpha()])
-
-    colreplacement = {'.* kN MTS Force':'force', 'Running Time':'runtime', 'Time':'time', '.* MTS Displacement':'displacement'}
+def force_displacement(filename, results_folder: str):
+    data = pd.read_csv(
+        f"../../data/{filename}", delimiter="\t", encoding="utf-8", header=2, on_bad_lines="skip"
+    )
+    data = data.drop(index=data.index[data["Time"].str.isalpha()])
+    colreplacement = {
+        ".* kN MTS Force": "force",
+        "Running Time": "runtime",
+        "Time": "time",
+        ".* MTS Displacement": "displacement",
+    }
     data.columns = data.columns.to_series().replace(colreplacement, regex=True)
 
     for column in data.columns:
-        data[column] = [x.replace(',', '.') for x in data[column]]
+        data[column] = [x.replace(",", ".") for x in data[column]]
         data[column] = data[column].astype(float)
 
-    data['force'], data['displacement'] = -data['force'], -data['displacement']
+    data["force"], data["displacement"] = -data["force"], -data["displacement"]
 
-    pl.plot(data['displacement'], data['force'])
-    pl.xlabel('Displacement [mm]')
-    pl.ylabel('Force [kN]')
-    pl.title('Placeholder')
-    pl.show()
-    #pl.savefig()
-    print('done')
+    plt.plot(data["displacement"], data["force"])
+    plt.xlabel("Displacement [mm]")
+    plt.ylabel("Force [kN]")
 
-force_displacement("specimen.dat")
+    format_plot_2d_linear()
+    save_plot(results_folder, name=f"force_displacement_{filename}", fig=plt)
