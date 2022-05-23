bbb3a7ba6f8229f4945d5e6f938b438a819d0dc5
Dominik Stiller <domi.stiller@gmail.com>
2022-05-23 17:37:38 +0200

    Omit clusterer during cluster index plotting if there is only one cluster

--- damage_identification/evaluation/cluster_indexes.py
+++ damage_identification/evaluation/cluster_indexes.py
@@ -13,58 +13,66 @@ from validclust import dunn
 from damage_identification.evaluation.plot_helpers import format_plot_2d, save_plot
 
 
+METRICS_NAMES = ["Davies", "Silhouette", "Dunn", "Calinski-Harabasz"]
+
+
 def graph_metrics(pipeline_dirs: Union[str, list]):
     if isinstance(pipeline_dirs, str):
         pipeline_dirs = pipeline_dirs.split(",")
 
-    n_pipelines = len(pipeline_dirs)
-
-    n_clusters = []
-    k_metrics = np.zeros((n_pipelines, 4))
-    f_metrics = np.zeros((n_pipelines, 4))
-    h_metrics = np.zeros((n_pipelines, 4))
+    pipeline_names = []
+    metrics = []
 
     for i, d in enumerate(pipeline_dirs):
         print(f"Processing {d}...")
         with open(os.path.join(d, "params.pickle"), "rb") as f:
-            n_clusters.append(pickle.load(f)["n_clusters"])
+            n_clusters = pickle.load(f)["n_clusters"]
+            pipeline_names.append(os.path.basename(d).replace("pipeline_", ""))
 
         # Only use subset of data since distance matrix is too large otherwise
         # No sampling necessary since training data are shuffled
-        data = pd.read_pickle(os.path.join(d, "training_features_pca.pickle.bz2")).head(n=10000)
+        data = pd.read_pickle(os.path.join(d, "training_features_pca.pickle.bz2")).head(n=30000)
+        metrics.append(_collate_metrics(data, d, n_clusters))
 
-        metrics = _collate_metrics(data, d).to_numpy()
+    metrics = pd.concat(metrics)
 
-        k_metrics[i] = metrics[0]
-        f_metrics[i] = metrics[1]
-        h_metrics[i] = metrics[2]
+    results_folder = os.path.join("data", "results", "indexes", "-".join(pipeline_names))
 
-    results_folder = os.path.join("data", "results", "indexes")
-
-    clusterer_names = ["kmeans", "fcmeans", "hierarchical"]
-    for m, name in zip([k_metrics, f_metrics, h_metrics], clusterer_names):
-        # Normalize indexes
-        m[:, 0] = np.abs((m[:, 0] - np.abs(np.max(m[:, 0]))) / np.max(m[:, 0]))
-        m[:, 1] = (m[:, 1] + 1) * 0.5
-        m[:, 3] = np.abs((m[:, 3] - np.min(m[:, 3])) / (np.max(m[:, 3]) - np.min(m[:, 3])))
-
+    for clusterer in metrics["clusterer"].unique():
         fig = plt.figure(figsize=(12, 6))
 
-        for i, label in enumerate(["Davies-Bouldin", "Silhouette", "Dunn", "Calinski-Harabasz"]):
-            plt.plot(n_clusters, k_metrics[:, i], label=label, marker="o")
+        for metric_name in METRICS_NAMES:
+            m = metrics[
+                (metrics["clusterer"] == clusterer) & (metrics["metric_name"] == metric_name)
+            ]
 
-        plt.xticks(n_clusters)
+            # Normalize indexes
+            if metric_name == "Davies":
+                m["metric_value"] = (
+                    (m["metric_value"] - abs(m["metric_value"].max())) / m["metric_value"].max()
+                ).abs()
+            elif metric_name == "Silhouette":
+                m["metric_value"] = (m["metric_value"] + 1) / 2
+            elif metric_name == "Calinski-Harabasz":
+                m["metric_value"] = (
+                    (m["metric_value"] - m["metric_value"].min())
+                    / (m["metric_value"].max() - m["metric_value"].min())
+                ).abs()
+
+            plt.plot(m["n_clusters"], m["metric_value"], label=metric_name, marker="o")
+
+        plt.xticks(metrics["n_clusters"].unique())
         plt.legend(loc=6)
         plt.ylabel("Index scores")
         plt.xlabel("k")
 
         format_plot_2d()
-        save_plot(results_folder, name, fig)
+        save_plot(results_folder, clusterer, fig)
 
     print(f"Saved results to {results_folder}")
 
 
-def _collate_metrics(data, directory):
+def _collate_metrics(data, directory, n_clusters):
     """
     Args:
         data: the training data from PCA with reduced features
@@ -77,20 +85,39 @@ def _collate_metrics(data, directory):
         data.to_numpy(), fastdist.euclidean, "euclidean", return_matrix=True
     )
 
-    k_labels = _load_labels(os.path.join(directory, "kmeans/model.pickle"), data.index)
-    k_metrics = _get_metrics(data, k_labels, distmatrix)
+    metrics = []
 
-    f_labels = _load_fcmeans_labels(os.path.join(directory, "fcmeans/fcmeans.pickle"), data)
-    f_metrics = _get_metrics(data, f_labels, distmatrix)
+    try:
+        k_labels = _load_labels(os.path.join(directory, "kmeans/model.pickle"), data.index)
+        metrics += [
+            {"metric_name": METRICS_NAMES[i], "metric_value": val, "clusterer": "kmeans"}
+            for i, val in enumerate(_get_metrics(data, k_labels, distmatrix))
+        ]
+    except:
+        print("Failed to collect kmeans metrics")
 
-    h_labels = _load_labels(os.path.join(directory, "hierarchical/hclust.pickle"), data.index)
-    h_metrics = _get_metrics(data, h_labels, distmatrix)
+    try:
+        f_labels = _load_fcmeans_labels(os.path.join(directory, "fcmeans/fcmeans.pickle"), data)
+        metrics += [
+            {"metric_name": METRICS_NAMES[i], "metric_value": val, "clusterer": "fcmeans"}
+            for i, val in enumerate(_get_metrics(data, f_labels, distmatrix))
+        ]
+    except:
+        print("Failed to collect fcmeans metrics")
 
-    return pd.DataFrame(
-        np.vstack((k_metrics, f_metrics, h_metrics)),
-        columns=["Davies", "Silhouette", "Dunn", "Calinski-Harabasz"],
-        index=["kmeans", "fcmeans", "hierarchical"],
-    )
+    try:
+        h_labels = _load_labels(os.path.join(directory, "hierarchical/hclust.pickle"), data.index)
+        metrics += [
+            {"metric_name": METRICS_NAMES[i], "metric_value": val, "clusterer": "hierarchical"}
+            for i, val in enumerate(_get_metrics(data, h_labels, distmatrix))
+        ]
+    except:
+        print("Failed to collect hierarchical metrics")
+
+    metrics = pd.DataFrame(metrics)
+    metrics["n_clusters"] = n_clusters
+
+    return metrics
 
 
 def _load_labels(directory, indices):
@@ -112,9 +139,12 @@ def _get_metrics(data, labels, distmatrix):
     silhouette = silhouette_score(data, labels)
     dunnmetric = dunn(distmatrix, labels)
     calinski = calinski_harabasz_score(data, labels)
-    return np.array([davies, silhouette, dunnmetric, calinski])
+    return [davies, silhouette, dunnmetric, calinski]
 
 
 if __name__ == "__main__":
+    # Prevent unnecessary warnings during index normalization
+    pd.options.mode.chained_assignment = None
+
     pipelines = sys.argv[1]
     graph_metrics(pipelines)
